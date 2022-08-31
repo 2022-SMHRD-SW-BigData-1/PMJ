012e603fa5b15e0656d9ee6173b96809e9b08a93
smhrd <smhrd@DESKTOP-5SATLC9>
2022-08-31 10:24:57 +0900

    두 번째 커밋입니다

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -4,7 +4,7 @@ public class Ex01 {
 	public static void main(String[] args) {
 
 		System.out.println("첫 번째 커밋!!"); // git
-		
+		System.out.println("두 번째 커밋!!"); // -> 해당 내용을 깃허브 사이트로 올려주세요(push!)
 	}
 
 }
